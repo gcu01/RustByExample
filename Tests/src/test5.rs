7b80da7fa463d63f6b225060b15b3666ae625bb6
Gabriel <gabrielcampeanu@gmail.com>
2024-12-10 12:45:16 +0100

    generics

--- Tests/src/test5.rs
+++ Tests/src/test5.rs
@@ -11,15 +11,39 @@ pub fn vector2() -> Vec<i32> {
 }
 
 pub fn largest<T : std::cmp::PartialOrd> (list: &[T]) -> &T {
-    let mut l = &list[0];
+    let mut lgst = &list[0];
 
     for i in list {
-        if i > l {
-            l = i;
+        if i > lgst {
+            lgst = i;
         }
     }
 
-    l
+    lgst
+}
+
+pub struct Figure <T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Figure<T, U> {
+    pub fn getx (&self) -> &T {
+        &self.x
+    }
+    pub fn gety(&self) -> &U {
+        &self.y
+    }
+    pub fn geta<A>(&self, a: A) -> A 
+    where A: Copy {
+        a
+    }
+}
+
+impl Figure<usize, usize> {
+    pub fn getxy(&self) -> (&usize, &usize) {
+        (&self.x, &self.y)
+    }
 }
 
 #[cfg(test)]
@@ -38,5 +62,32 @@ mod test {
     fn largest_test() {
         let v1 = vec![1, 2, 5, 7, 11];
         assert_eq!(11, *largest(&v1));
+    }
+
+    #[test]
+    fn figure_test_x() {
+        let f: Figure<i32, usize> = Figure {x: 10, y: 11};
+        assert_eq!(*f.getx(), 10);
+
+    }
+
+    #[test]
+    fn figure_test_y() {
+        let f: Figure<i32, usize> = Figure {x: 10, y: 11};
+        assert_eq!(*f.gety(), 11);
+
+    }
+
+    #[test]
+    fn figure_test_xy() {
+        let f: Figure<usize, usize> = Figure {x: 10, y: 11};
+        assert_eq!(f.getxy(), (&10, &11));
+
+    }
+
+    #[test]
+    fn figure_test_a(){
+        let f:Figure<i32, i32> = Figure {x:1,y:2};
+        assert_eq!(f.geta(10), 10);
     }
 }
